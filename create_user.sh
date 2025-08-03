c3dabc75d45c83dbb854fef28bc69bbc35cf24a6
aaron <caaronh85@gmail.com>
2025-08-03 00:47:54 -0400

    added all base files for user create script.

---
+++ create_user.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+x=1
+
+create_uname () {
+	local fname="$1"
+	local lname="$2"
+	echo $fname | tr '[:upper:]' '[:lower:]'
+	echo $lname | tr '[:upper:]' '[:lower:]'
+}
+while [ $x -le 5 ]
+	do
+	echo "New user provisioning script."
+	read -p "Enter employee first name: " fname
+	read -p "Enter employee last name: " lname
+	read -p "Is this correct?Y/N " create
+	if [ $create = "Y" ]
+	then	
+		create_uname "$fname" "$lname"
+		((x++))
+		echo "user created"
+	
+	else
+		echo "nope"
+	fi
+done
+
+
